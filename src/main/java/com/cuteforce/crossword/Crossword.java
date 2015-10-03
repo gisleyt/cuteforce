be48aec734c14c1720bd3d8bfdc1ab4f4930002e
Gisle Ytrestol <gisle.ytrestol@cxense.com>
2015-10-03 23:45:28 +0200

    Remove redundant list import after letterprob optimize

--- src/main/java/com/cuteforce/crossword/Crossword.java
+++ src/main/java/com/cuteforce/crossword/Crossword.java
@@ -2,7 +2,6 @@ package com.cuteforce.crossword;
 
 import com.google.common.base.MoreObjects;
 import com.google.common.collect.ComparisonChain;
-import com.google.common.collect.Lists;
 
 import com.cuteforce.crossword.Dictionary.Node;
 
