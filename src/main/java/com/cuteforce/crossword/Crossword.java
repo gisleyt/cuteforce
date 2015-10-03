6c68968062c343700b559484de06117b99a8e150
Gisle Ytrestol <gisle.ytrestol@cxense.com>
2015-10-03 23:41:42 +0200

    Optimize LetterProbability computation
    Avoid calling Node.getNode() all the time.

--- src/main/java/com/cuteforce/crossword/Crossword.java
+++ src/main/java/com/cuteforce/crossword/Crossword.java
@@ -1,16 +1,43 @@
 package com.cuteforce.crossword;
 
 import com.google.common.base.MoreObjects;
+import com.google.common.collect.ComparisonChain;
+import com.google.common.collect.Lists;
+
 import com.cuteforce.crossword.Dictionary.Node;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Crossword {
 
     private int size;
     private Dictionary dictionary;
+
+    public static class LetterProb {
+        public final double probability;
+        public final Character character;
+
+        public LetterProb(double probability, Character character) {
+            this.probability = probability;
+            this.character = character;
+        }
+
+        public static final Comparator<LetterProb> LETTERPROB_COMPARATOR = new Comparator<LetterProb>() {
+            @Override
+            public int compare(LetterProb l1, LetterProb l2) {
+                return ComparisonChain.start()
+                        .compare(l2.probability, l1.probability)
+                        .compare(l1.character, l2.character)
+                        .result();
+            }
+        };
+    }
 
     public Crossword(int size, File dictionary) throws IOException {
         this.size = size;
@@ -51,15 +78,18 @@ public class Crossword {
         }
 
         Map<Character, Double> verticalProbs = this.dictionary.getProb(vertical, this.size);
-        double maxProb = 0.0;
-        Character maxLetter = null;
-        for (Character letter : horizontalProbs.keySet()) {
-            double prob = horizontalProbs.get(letter) * MoreObjects.firstNonNull(verticalProbs.get(letter), 0.0);
-            if (maxProb < prob && failedPaths.getNode(letters + letter.toString()) == null) {
-                maxLetter = letter;
-                maxProb = prob;
+        List<LetterProb> letterProbabilities = horizontalProbs.keySet()
+            .stream()
+            .map(letter -> new LetterProb(horizontalProbs.get(letter) * MoreObjects.firstNonNull(verticalProbs.get(letter), 0.0), letter))
+            .filter(letterProb -> letterProb.probability > 0.0)
+            .collect(Collectors.toList());
+
+        Collections.sort(letterProbabilities, LetterProb.LETTERPROB_COMPARATOR);
+        for (LetterProb lp : letterProbabilities) {
+            if (failedPaths.getNode(letters + lp.character.toString()) == null) {
+                return lp.character;
             }
         }
-        return maxLetter;
+        return null;
     }
 }
