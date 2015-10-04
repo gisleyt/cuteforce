f56654d14c3ca37d57aae2a7f74327aed26efa12
Gisle Ytrestol <gisle.ytrestol@cxense.com>
2015-10-04 18:12:02 +0200

    Create proper test

--- src/test/java/com/cuteforce/crossword/TestCrossword.java
+++ src/test/java/com/cuteforce/crossword/TestCrossword.java
@@ -4,6 +4,7 @@ import com.google.common.io.Resources;
 
 import com.cuteforce.crossword.Crossword;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -13,9 +14,20 @@ public class TestCrossword {
 
     @Test
     public void testCrossword() throws IOException {
+        Crossword cw = new Crossword(4, new File(Resources.getResource(TestCrossword.class, "lemmatization.txt").getFile()));
+        String grid = cw.solve(true);
+        Assert.assertEquals(grid, "sarsaloeroseseen");
+        cw = new Crossword(5, new File(Resources.getResource(TestCrossword.class, "lemmatization.txt").getFile()));
+        grid = cw.solve(true);
+        Assert.assertEquals(grid, "starstarotantueagutitosen");
+    }
+
+    @Test
+    public void testSolve() throws IOException {
         int size = 7;
         Crossword cw = new Crossword(size, new File(Resources.getResource(TestCrossword.class, "lemmatization.txt").getFile()));
         String grid = cw.solve(true);
+
         for (int i = 0; i < grid.length(); i++) {
             if (i % size == 0) {
                 System.out.print("\n");
@@ -24,6 +36,4 @@ public class TestCrossword {
         }
         System.out.print("\n");
     }
-
-
 }
